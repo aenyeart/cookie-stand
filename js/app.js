95220bb6bfe14310d1732acb480970f7818751d8
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-01 22:35:30 -0700

    Generates salesSheet list for one location

--- js/app.js
+++ js/app.js
@@ -1,31 +1,58 @@
 'use strict'
+const storeHours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm"
+  ]
 
 function calcTotalSales(storeObj) {
   for (let i = 0; i < storeObj.hourlySales.length; i++) {
     storeObj.totalSales += storeObj.hourlySales[i];
   }
-   
 }
 
 function calcHourlySales(storeObj) {
-  for (let i = 0; i < storeObj.storeHours; i++) {
+  for (let i = 0; i < storeHours.length; i++) {
     let hourlyCust = randoNum(storeObj.minCust, storeObj.maxCust);
     storeObj.hourlySales[i] = Math.round(storeObj.avgSoldPer * hourlyCust);
   }
 }
 
-const storeLocation = { //really wish
-  storeHours: 14,
+const storeObj = { //
   minCust: 23,
   maxCust: 65,
   avgSoldPer: 6.3,
   hourlySales: [],
   totalSales: 0
 };
-calcHourlySales(storeLocation);
-calcTotalSales(storeLocation);
+
+calcHourlySales(storeObj);
+calcTotalSales(storeObj);
 
 function randoNum(min, max) {
   let range = max - min + 1;
   return (Math.floor(Math.random() * range) + min);
+}
+
+const salesSheet = document.getElementById("sales-sheet");
+const listOfSales = document.createElement("ul");
+
+salesSheet.appendChild(listOfSales);
+
+for (let i = 0; i < storeHours.length; i++) {
+  const listElem = document.createElement("li");
+  let salesEntry = `${storeHours[i]}: ${storeObj.hourlySales[i]} cookies`;
+  listOfSales.appendChild(listElem);
+  listElem.textContent = salesEntry;
 }
