1c06db8486f87598477cc8bf01b4a7623e1e9077
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-02 11:16:45 -0700

    Rewrites storeBuilder as Store constructor function

--- js/app.js
+++ js/app.js
@@ -17,19 +17,18 @@ const storeHours = [
   "7pm"
 ];
 
-function storeBuilder(location, minCust, maxCust, avgSoldPer) { // MAIN FUNCTION
-  
-  const storeObj = { // OBJECT LITERAL
-    location,
-    minCust,
-    maxCust,
-    avgSoldPer,
-    hourlySales: [],
-    totalSales: 0
-  };
-  calcHourlySales(storeObj);
-  calcTotalSales(storeObj);
-  buildSalesSheet(storeObj);
+function Store(location, minCust, maxCust, avgSoldPer) { // Construct FUNCTION
+
+  this.location = location, 
+  this.minCust = minCust,
+  this.maxCust = maxCust,
+  this.avgSoldPer = avgSoldPer,
+  this.hourlySales = [],
+  this.totalSales = 0,
+
+  this.calcHourlySales();
+  this.calcTotalSales();
+  this.buildSalesSheet();
 
 } // END STOREBUILDER FUNCTION -- called at bottom of app.js
 
@@ -38,43 +37,41 @@ function randoNum(min, max) {
   return (Math.floor(Math.random() * range) + min);
 }
 
-function calcHourlySales(storeObj) {
+Store.prototype.calcHourlySales = function () {
   for (let i = 0; i < storeHours.length; i++) {
-    let hourlyCust = randoNum(storeObj.minCust, storeObj.maxCust);
-    storeObj.hourlySales[i] = Math.round(storeObj.avgSoldPer * hourlyCust);
+    let hourlyCust = randoNum(this.minCust, this.maxCust);
+    this.hourlySales[i] = Math.round(this.avgSoldPer * hourlyCust);
+  }
+}
+Store.prototype.calcTotalSales = function () {
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    this.totalSales += this.hourlySales[i];
   }
 }
 
-function calcTotalSales(storeObj) {
-  for (let i = 0; i < storeObj.hourlySales.length; i++) {
-    storeObj.totalSales += storeObj.hourlySales[i];
-  }
-}
-
-
-function buildSalesSheet(storeObj) {
+Store.prototype.buildSalesSheet = function () {
   const salesSheet = document.getElementById("sales-sheet"); // Grab PARENT 
 
   const location = document.createElement("h2"); // Create HEADING
   salesSheet.appendChild(location);
-  location.textContent = storeObj.location;
-  
+  location.textContent = this.location;
+
   const listOfSales = document.createElement("ul"); // Create LIST
   salesSheet.appendChild(listOfSales);
 
-  for (let i = 0; i < storeHours.length; i++) {  // Create LIST ITEMS
+  for (let i = 0; i < storeHours.length; i++) { // Create LIST ITEMS
     const listElem = document.createElement("li");
-    let salesEntry = `${storeHours[i]}: ${storeObj.hourlySales[i]} cookies`;
+    let salesEntry = `${storeHours[i]}: ${this.hourlySales[i]} cookies`;
     listOfSales.appendChild(listElem);
     listElem.textContent = salesEntry;
   }
-  const totalCookies = document.createElement("li");  //  TOTAL
+  const totalCookies = document.createElement("li"); //  TOTAL
   listOfSales.appendChild(totalCookies);
-  totalCookies.textContent = `Total: ${storeObj.totalSales} cookies`;
+  totalCookies.textContent = `Total: ${this.totalSales} cookies`;
 }
 
-storeBuilder("Seattle", 23, 65, 6.3);
-storeBuilder("Tokyo", 3, 24, 1.2);
-storeBuilder("Dubai", 11, 38, 3.7);
-storeBuilder("Paris", 20, 38, 2.3);
-storeBuilder("Lima", 2, 16, 4.6);
+const seattle = new Store("Seattle", 23, 65, 6.3);
+const tokyo = new Store("Tokyo", 3, 24, 1.2);
+const dubai = new Store("Dubai", 11, 38, 3.7);
+const paris = new Store("Paris", 20, 38, 2.3);
+const lima = new Store("Lima", 2, 16, 4.6);
